d2a7bc7df034be0f0f573e265af757b5457c2cc5
Aashikajain1 <136193079+Aashikajain1@users.noreply.github.com>
2023-07-21 17:55:37 +0530

    Add files via upload

---
+++ clndr.js
@@ -0,0 +1,55 @@
+var dt = new Date();
+function cal() {
+    document.getElementById("tod").innerHTML = dt.getFullYear();
+    dt.setDate(1);
+    var day = dt.getDay();
+    console.log(dt.getDay());
+
+    var enddt = new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate();
+    var prdt = new Date(dt.getFullYear(), dt.getMonth(), 0).getDate();
+
+    var month = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY",
+        "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"];
+    document.getElementById("mnth").innerHTML = month[dt.getMonth()];
+
+    var today = new Date();
+    console.log(today);
+    var d = "";
+    for (x = day; x > 0; x--) {
+        d += "<div class='pdt'>" + (prdt - x + 1) + "</div>";
+    }
+    for (i = 1; i <= enddt; i++) {
+        if (i == today.getDate() && dt.getMonth() == today.getMonth()) {
+            d += "<div class='tdy'>" + i + "</div>";
+        }
+        else {
+            d += "<div>" + i + "</div>";
+        }
+
+    }
+    document.getElementsByClassName("dates")[0].innerHTML = d;
+
+}
+function mvdt(arg) {
+    if (arg == 'p') {
+        dt.setMonth(dt.getMonth() - 1);
+    }
+    else if (arg == 'n') {
+        dt.setMonth(dt.getMonth() + 1);
+    }
+    cal();
+}
+function listv(arg){
+    if(arg=='y'){
+        var yr;    
+        yr=Number(prompt("Enter Year"));
+        
+        
+    }
+    else if(arg=='m'){
+        var yr;
+        yr=Number(prompt("Enter Month"));
+        dt.setMonth(dt.getMonth(yr)-1);
+    }
+    cal();
+}
